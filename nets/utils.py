11838b9648ca339431a02ec3219a1c45086134ae
G <gauthier.castro@cfms.org.uk>
2020-12-04 17:39:17 +0000

    add CustomSubset class

--- nets/utils.py
+++ nets/utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
 
 
 def plot_loss(results):
@@ -12,3 +13,29 @@ def plot_loss(results):
     plt.xlabel("number of training examples seen")
     plt.ylabel("negative log likelihood loss")
     return fig
+
+
+class CustomSubset(Dataset):
+    r"""
+    Subset of a dataset at specified indices with given transform.
+    Same as torch.utils.data.Subset but with transform support.
+
+    Arguments:
+        dataset (Dataset): The whole Dataset
+        indices (sequence): Indices in the whole set selected for subset
+        transform (Transform): Transform operation to be applied to the dataset.
+    """
+
+    def __init__(self, dataset, indices, transform=None):
+        self.dataset = dataset
+        self.indices = indices
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        x, y = self.dataset[self.indices[idx]]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
+
+    def __len__(self):
+        return len(self.indices)
